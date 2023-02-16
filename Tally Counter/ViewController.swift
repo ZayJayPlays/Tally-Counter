4fd13b3fcc214e675de1eb0f55d58c4027d1de58
ZayJayPlays <116956084+ZayJayPlays@users.noreply.github.com>
2023-02-16 09:10:30 -0700

    First Commit

--- Tally Counter/ViewController.swift
+++ Tally Counter/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentCount = 0
+    var incrementAmount = 1
+    @IBOutlet var countLabel: UILabel!
+    @IBOutlet var incrementLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateTexts()
     }
 
-
+    @IBAction func ResetButton(_ sender: UIButton) {
+        currentCount = 0
+        incrementAmount = 1
+        updateTexts()
+    }
+    
+    @IBAction func increaseCount(_ sender: UIButton) {
+        currentCount += incrementAmount
+        updateTexts()
+    }
+    
+    @IBAction func decreaseCount(_ sender: UIButton) {
+        currentCount -= incrementAmount
+        updateTexts()
+    }
+    
+    @IBAction func increaseIncrement(_ sender: UIButton) {
+        incrementAmount += 1
+        updateTexts()
+    }
+    
+    @IBAction func decreaseIncrement(_ sender: UIButton) {
+        incrementAmount -= 1
+        updateTexts()
+    }
+    
+    func updateTexts() {
+        countLabel.text = String(currentCount)
+        incrementLabel.text = String(incrementAmount)
+    }
+    
 }
 
